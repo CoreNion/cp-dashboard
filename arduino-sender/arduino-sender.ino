01b2d4130b75534be260c6053bd0129e0f372a15
CoreNion <coreinion@gmail.com>
2024-01-09 12:09:35 +0900

    [arduino] BME680からセンサー情報を取得するように変更

--- arduino-sender/arduino-sender.ino
+++ arduino-sender/arduino-sender.ino
@@ -1,48 +1,67 @@
+/**
+ * BME680から気温・湿度・気圧を測定し、JSON形式でシリアル出力するArduinoスケッチ
+ * (Adafruit BME680 Libraryを使用)
+ * 
+ * Arduinoにこのスケッチを書き込んだ後、BME680をI2C接続し、USBでPCに接続するだけで、ダッシュボードで利用できるようになります！
+ * 開発時に利用したモジュールキット: AE-BME680 (https://akizukidenshi.com/catalog/g/gK-14469/)
+ */
+
 #include <Wire.h>
 #include <ArduinoJson.h>
+#include <Adafruit_BME680.h>
 
-const int TMP36_APIN = 0;
-
-// -OutPutと接続
-const int MPS2407_MOUTPUT_APIN = 1;
-// +OutPutと接続
-const int MPS2407_POUTPUT_APIN = 2;
+Adafruit_BME680 bme; // I2C
 
 void setup()
 {
   Serial.begin(115200);
+
+  // bme680の初期化
+  if (!bme.begin())
+  {
+    // 初期化失敗時の処理 (シリアル接続後にエラーを表示して終了)
+    while (!Serial)
+      ;
+    Serial.println("[e] Could not find a valid BME680 sensor!");
+    while (1);
+  }
+
+  // oversampling/filterを設定
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
 }
 
 void loop()
 {
   if (Serial.available())
   {
+    // シリアルから"REQUEST_SENSOR_DATA;"が送られてきたら、センサーからデータを取得してJSONを出力する
     String data = Serial.readStringUntil(';');
     if (data == "REQUEST_SENSOR_DATA")
     {
-      // TMP36 温度測定
-      // データシート: https://www.analog.com/media/jp/technical-documentation/data-sheets/TMP35_36_37_jp.pdf
-      float tmpVoltage = analogRead(TMP36_APIN) * 5.0 / 1023.0;
-      // 気温 = (温度センサーの出力電圧 - オフセット電圧 500mv) * 100
-      float temperature = (tmpVoltage - 0.5) * 100;
+      // センサーからデータを取得
+      if (!bme.performReading())
+      {
+        // 取得失敗時の処理 (エラーを表示して終了)
+        Serial.println("[e] Failed to perform reading :(");
+        return;
+      }
 
-      // MPS2407 気圧測定 (WIP)
-      // データシート: https://akizukidenshi.com/download/ds/metrodyne/MPS-2400series.pdf
-      // ホイートストンブリッジ回路が使われている模様、二つの出力電圧の差分(スパン電圧)から、気圧を算出する
+      // 温度
+      float temperature = bme.temperature;
 
-      // -Outputの電圧 (面倒なのでmvに変換)
-      float mps2407MOutVoltage = analogRead(MPS2407_MOUTPUT_APIN) * 5.0 / 1023.0 * 1000;
-      // +Outputの電圧 (mvに変換)
-      float mps2407POutVoltage = analogRead(MPS2407_POUTPUT_APIN) * 5.0 / 1023.0 * 1000;
-      // 気圧 = (P-Outputの電圧 - M-Outputの電圧) / 0.174 (推測、WIP)
-      // Arduinoのアナログ入力の分解能が低すぎて、気圧の算出がうまくいかない模様
-      float pressure = (mps2407POutVoltage - mps2407MOutVoltage) / 0.174;
+      // 湿度
+      float humidity = bme.humidity;
+
+      // 気圧 (hPa)
+      float pressure = bme.pressure / 100.0;
 
       // JSONを生成
       DynamicJsonDocument doc(200);
       doc["temperature"] = temperature;
-      doc["mps2407MOutVoltage"] = mps2407MOutVoltage;
-      doc["mps2407POutVoltage"] = mps2407POutVoltage;
+      doc["humidity"] = humidity;
       doc["pressure"] = pressure;
 
       // JSON出力
