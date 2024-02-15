949233590b2b114f2e41e7091994c94073e7d4ab
CoreNion <coreinion@gmail.com>
2024-02-15 15:34:10 +0900

    [setting] 一般的な設定を別ファイルに分離

--- components/modals/settings.vue
+++ components/modals/settings.vue
@@ -1,15 +1,4 @@
 <script setup lang="ts">
-// テーマ設定
-const colorMode = useColorMode();
-
-// センサーのソース
-const isSnowEnabledState = isSnowEnabled();
-
-// 情報スクロールの有効化
-const isInfoScrollEnabledSt = isInfoScrollEnabled();
-// 情報スクロールに表示する文字列
-const infoScrollTextSt = infoScrollText();
-
 // チャイムの音源
 const chimeSourceState = chimeSource();
 // チャイムのファイル名
@@ -83,43 +72,13 @@ const playAudio = (link: string) => {
   audio.volume = 1.0;
   audio.play();
 }
-
-// 雪の設定
-const onSnowModeChange = async (e: Event) => {
-  if (!(e.target instanceof HTMLInputElement)) return;
-  const value = e.target.checked;
-
-  // @ts-ignore
-  await import("pure-snow.js").then(({ createSnow, showSnow }) => {
-    showSnow(value);
-  });
-}
 </script>
 
 <template>
   <div>
     <h3 class="font-bold text-lg">設定</h3>
     <p class="py-2">
-      <label class="label">
-        <span class="label-text">テーマ</span>
-        <select class="select select-bordered w-full max-w-xs" v-model="colorMode.preference">
-          <option disabled selected>テーマを選択...</option>
-          <option v-for="theme of themes" :key="theme">{{ theme }}</option>
-        </select>
-      </label>
-
-      <label class="label cursor-pointer mt-4">
-        <span class="label-text">情報スクロールの有効化</span>
-        <input type="checkbox" class="toggle toggle-secondary" v-model="isInfoScrollEnabledSt" />
-      </label>
-      <input v-if="isInfoScrollEnabledSt" type="text" placeholder="スクロールに表示される文章を入力..."
-        class="input input-bordered w-full" v-model="infoScrollTextSt" />
-
-      <label class="label cursor-pointer mt-4">
-        <span class="label-text">Snow❄︎</span>
-        <input type="checkbox" class="toggle toggle-secondary" @change="onSnowModeChange" v-model="isSnowEnabledState" />
-      </label>
-
+      <GeneralSetting></GeneralSetting>
       <WeatherSetting></WeatherSetting>
     </p>
 
---
+++ components/setting/general-setting.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+// テーマ設定
+const colorMode = useColorMode();
+
+// センサーのソース
+const isSnowEnabledState = isSnowEnabled();
+
+// センサー情報を表示するかどうか
+const sensorVisible = isSensorInfoVisible();
+// 情報スクロールの有効化
+const isInfoScrollEnabledSt = isInfoScrollEnabled();
+// 情報スクロールのテキスト
+const infoScrollTextSt = infoScrollText();
+
+// 雪の設定
+const onSnowModeChange = async (e: Event) => {
+  if (!(e.target instanceof HTMLInputElement)) return;
+  const value = e.target.checked;
+
+  // @ts-ignore
+  await import("pure-snow.js").then(({ createSnow, showSnow }) => {
+    showSnow(value);
+  });
+}
+</script>
+
+<template>
+  <label class="label">
+    <span class="label-text">テーマ</span>
+    <select class="select select-bordered max-w-xs" v-model="colorMode.preference">
+      <option disabled selected>テーマを選択...</option>
+      <option v-for="theme of themes" :key="theme">{{ theme }}</option>
+    </select>
+  </label>
+
+  <label class="label cursor-pointer">
+    <span class="label-text">センサーからの情報を表示</span>
+    <input type="checkbox" class="toggle toggle-secondary" v-model="sensorVisible" />
+  </label>
+  <label class="label cursor-pointer">
+    <span class="label-text">情報スクロールの有効化</span>
+    <input type="checkbox" class="toggle toggle-secondary" v-model="isInfoScrollEnabledSt" />
+  </label>
+  <input v-if="isInfoScrollEnabledSt" type="text" placeholder="スクロールに表示される文章を入力..." class="input input-bordered w-full"
+    v-model="infoScrollTextSt" />
+
+  <label class="label cursor-pointer">
+    <span class="label-text">Snow❄︎</span>
+    <input type="checkbox" class="toggle toggle-secondary" @change="onSnowModeChange" v-model="isSnowEnabledState" />
+  </label>
+</template>
