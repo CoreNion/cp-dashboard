b0391d6d585b5a75f7afae48de3c5c34e98663c9
CoreNion <coreinion@gmail.com>
2024-02-27 14:35:44 +0900

    [countdown] 選択されたイベントの削除などを実装

--- components/modals/countdown-setting.vue
+++ components/modals/countdown-setting.vue
@@ -1,24 +1,45 @@
 <script setup lang="ts">
+import dayjs from 'dayjs';
+
 const countdownDatesState = countdownDates();
 
-const selectedEventColor = ref<string>("blue");
+const selectedEventColor = ref<string>("gray");
 const eventLabel = ref<string>("");
-const selectedDate = ref(new Date());
+const selectedDate: Ref<Date | null> = ref(new Date());
+
 const attr = ref<any[]>([]);
 
 const onDateSettings = () => {
   countdownDatesState.value = [
     ...(countdownDatesState.value),
     {
-      date: selectedDate.value.toISOString(),
+      date: dayjs(selectedDate.value!).format("YYYY-MM-DD"),
       label: eventLabel.value,
       color: selectedEventColor.value,
     },
   ];
 };
 
+const removeDate = (date: string) => {
+  countdownDatesState.value = countdownDatesState.value.filter((d) => d.date !== date);
+  eventLabel.value = "";
+  selectedDate.value = null;
+};
+
 watch(countdownDatesState, () => {
   refleshCountDownDate();
+});
+
+watch(selectedDate, () => {
+  eventLabel.value = "";
+  selectedEventColor.value = "gray";
+  for (const date of countdownDatesState.value) {
+    if (dayjs(selectedDate.value!).format("YYYY-MM-DD") === date.date) {
+      eventLabel.value = date.label;
+      selectedEventColor.value = date.color;
+      break;
+    }
+  }
 });
 
 const refleshCountDownDate = () => {
@@ -43,20 +64,32 @@ onMounted(() => {
 </script>
 
 <template>
+  <form method="dialog">
+    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+  </form>
+
   <h3 class="font-bold text-lg">カウントダウン設定</h3>
   <p class="py-2">
-    <VDatePicker v-model="selectedDate" expanded title-position="left" :attributes="attr" color="orange" />
+    <VDatePicker v-model="selectedDate" expanded title-position="left" :attributes="attr" color="orange">
+      <template #footer>
+        <span class="block p-2 border-t w-full">{{ selectedDate === null ? "未選択" :
+          dayjs(selectedDate).format("YYYY年MM月DD日") }}</span>
+      </template>
+    </VDatePicker>
   </p>
   <p class="py-2">
-    <span class="join-item select ">{{ selectedDate }}</span>
   <div class="join w-full">
+    <button class="join-item btn btn-error" @click="removeDate(dayjs(selectedDate).format('YYYY-MM-DD'))">
+      <Icon name="uil:trash-alt" size="2.5vh" />
+    </button>
     <select class="join-item select select-bordered border" v-model="selectedEventColor">
       <option v-for="color of ['gray', 'red', 'yellow', 'green', 'teal', 'blue', 'indigo', 'purple', 'pink']">
         {{ color }}
       </option>
     </select>
     <input class="join-item input input-bordered w-full" placeholder="日付のタイトル" v-model="eventLabel" />
-    <button class="join-item btn btn-primary" @click="onDateSettings">設定</button>
+    <button :class="['join-item', 'btn', 'btn-primary', selectedDate === null ? 'btn-disabled' : '']"
+      @click="onDateSettings">設定</button>
   </div>
   </p>
 </template>
